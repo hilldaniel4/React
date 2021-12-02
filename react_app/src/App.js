6db1ebe82ae917f28410a3e9ba0138978117d74b
hilldaniel4 <hill_daniel4@columbusstate.edu>
2021-12-02 18:39:19 +0000

    React 5 completed.

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,12 +1,17 @@
 import {Component} from 'react';
+import Cards from "./Cards";
+import { Jumbotron } from 'reactstrap';
 import './App.css';
-import { Button, Card, CardBody, CardTitle, Jumbotron } from 'reactstrap';
+import MyCarousel from './MyCarousel';
+import MyNav from './MyNav';
+import MyDropdown from './MyDropdown';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      showSecondCard: false
+      fade1: false,
+      fade2: false
     }
   }
 
@@ -14,28 +19,32 @@ class App extends Component {
     let cname = "lead";
     return(
       <div>
+        <MyNav />
         <Jumbotron>
           <h1 className={"display-3"}>My First React App</h1>
           <p className={cname}>This is my first attempt at it</p>
           <hr className={"my-2"}/>
           <p>Playing with reactstrap</p>
+            <MyCarousel>
+            </MyCarousel>
           <p className={cname}></p>
         </Jumbotron>
-        <Card key = "firstCard">
-          <CardBody>
-            <CardTitle>My Card</CardTitle>
-            <Button onClick={() => this.setState({ showSecondCard : true })}>Button</Button>
-          </CardBody>
-        </Card>
-        {this.state.showSecondCard ? <Card>
-          <CardBody>
-            <CardTitle>My Second Card</CardTitle>
-            <Button onClick={() => this.setState({ showSecondCard : false })}>Button</Button>
-          </CardBody>
-        </Card> : null}
+        <Cards 
+        fade1={this.state.fade1} cardOneState={this.cardOneState}
+        fade2={this.state.fade2} cardTwoState={this.cardTwoState}/>
+        <MyDropdown/>
       </div>
     )
   }
+  cardOneState = () => {
+    this.setState({ fade1 : !this.state.fade1 })
+  }
+
+  cardTwoState = () => {
+    this.setState({ fade2 : !this.state.fade2 })
+  }
+
 }
+
 
 export default App;
---
+++ react_app/src/MyCarousel.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+
+const items = [
+  {
+    src: 'https://images.unsplash.com/photo-1635623014674-58ec02a13d2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    altText: 'Slide 1',
+    caption: 'Slide 1'
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1635612717838-be24c0a2a424?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+    altText: 'Slide 2',
+    caption: 'Slide 2'
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1635709045508-c07a580c97b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+    altText: 'Slide 3',
+    caption: 'Slide 3'
+  }
+];
+
+const MyCarousel = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+        <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+    </Carousel>
+  );
+}
+
+export default MyCarousel;
